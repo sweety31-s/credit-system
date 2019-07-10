e14e61bde64a77ffaf2372c25c419b68bf928afd
sweety31-s <51282363+sweety31-s@users.noreply.github.com>
2019-07-10 21:24:33 +0530

    update dbconnect.php

--- credits_management/php/db_connect.php
+++ credits_management/php/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 	//make connection with database
-	$link = mysqli_connect("localhost","root","","credit");
+	$link = mysqli_connect("db4free.net","ssweety","password","sweetblue");
 	// Check connection
 	if($link === false) {
     	die("ERROR: Could not connect. " . mysqli_connect_error());
