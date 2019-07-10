567b9da2d4f7b7cf39e17528486cf0a31ea5abd8
Sweety <ssweety9643@gmail.com>
2019-07-10 13:34:08 +0530

    cedit

---
+++ credits_management/credit.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 14, 2018 at 12:46 PM
+-- Server version: 5.7.21-0ubuntu0.16.04.1
+-- PHP Version: 7.0.25-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `credit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transfers`
+--
+
+CREATE TABLE `transfers` (
+  `from_user` varchar(20) DEFAULT NULL,
+  `to_user` varchar(20) DEFAULT NULL,
+  `credits_tr` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transfers`
+--
+
+INSERT INTO `transfers` (`from_user`, `to_user`, `credits_tr`) VALUES
+('Maria', 'Alena', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `name` varchar(20) DEFAULT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `credit` int(11) DEFAULT '10'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`name`, `email`, `credit`) VALUES
+('Ross', 'ross@gmail.com', 10),
+('Jordan', 'jordan@yahoo.com', 10),
+('Alena', 'alena@yahoo.com', 11),
+('Maria', 'maria@gmail.com', 9),
+('Scott', 'scott@hotmail.com', 10),
+('Max', 'max@yahoo.com', 10),
+('Richard', 'richard@gmail.com', 10),
+('Britney', 'britney@hotmail.com', 10),
+('Isabell', 'isabell@yahoo.com', 10),
+('Charlie', 'charlie@gmail.com', 10);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
